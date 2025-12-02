805054b52292f9a9ce88b48676da2432990de27b
Denis <devi.com@gmail.com>
2025-12-02 03:39:56 +0300

    dzzz

--- cnt-group/src/app/app.module.ts
+++ cnt-group/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './@theme/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
